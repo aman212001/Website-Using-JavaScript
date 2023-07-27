682b4bfbc0928ab533dea122e9c863278bdff031
Aman Sharma <aman.s212001@gmail.com>
2023-07-27 14:43:50 +0530

    Added Form Section with Validation

--- assests/JS/script.js
+++ assests/JS/script.js
@@ -1,1 +1,19 @@
+// Form Validation
+function validate()
+{
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
+
+if(username=="admin" && password=="123")
+{
+    alert("Getting you in!!!");
+    return false;
+}
+else
+{
+    alert("Invalid Username or Password");
+    return false;
+}
+
+}
